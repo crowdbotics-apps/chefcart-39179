985590fda1517b10200d8156101fdfbdf56e21e3
Crowdbotics <team@crowdbotics.com>
2023-03-01 18:22:12 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "chefcart-39179",
+  url: "https://chefcart-39179.botics.co",
+  api: "https://chefcart-39179.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
